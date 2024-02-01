72cd0649aff43a0b166703342a8785e911ba6cbe
Oscar <osm1019@gmail.com>
2024-01-31 18:05:42 -0800

    Revert "shusky: Ship PixelParts"
    This reverts commit 8568fb0e07d6f36a3e5856146ffc5ad3668a497c.

--- device-lineage.mk
+++ device-lineage.mk
@@ -18,9 +18,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
 
-# PixelParts
-include packages/apps/PixelParts/device.mk
-
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
 
